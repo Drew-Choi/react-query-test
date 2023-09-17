27318c714220f8e2637de4981d652f853a24ef4c
Drew <drc920630@gmail.com>
2023-09-17 23:57:41 +0900

    무한스크롤 리액트쿼리

---
+++ second_infinit/src/species/Species.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+export function Species({
+  name,
+  language,
+  averageLifespan,
+}: {
+  name: string;
+  language: string;
+  averageLifespan: string;
+}) {
+  return (
+    <li>
+      {name}
+      <ul>
+        <li>language: {language}</li>
+        <li>average lifespan: {averageLifespan}</li>
+      </ul>
+    </li>
+  );
+}
---
+++ second_infinit/src/types.d.ts
@@ -0,0 +1,14 @@
+interface DataTypes {
+  userId: number;
+  id: number;
+  title?: string;
+  body?: string;
+}
+
+interface CommentsType {
+  postId: number;
+  id: number;
+  name?: string;
+  email?: string;
+  body?: string;
+}
